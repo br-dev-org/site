56d7ba2db86169f809306393fdf44eb40a4bb016
Pablo R. Dinella <pablo.dinella@gmail.com>
2023-01-13 10:11:02 -0300

    add: Imagens opengraph automáticas

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import sitemap from "@astrojs/sitemap";
+import astroOGImage from "astro-og-image";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://br-dev.org",
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(),
+    sitemap(),
+    astroOGImage({
+      config: {
+        path: "/publicacoes", // change this value to the folder where your posts are
+        // NOTE: index.md file will not get proccesed, so please avoid it
+      },
+    }),
+  ],
 });
