4ad87f0b16075b36371e07dd1b21bacc48a346bb
PabloDinella <pablo.dinella@gmail.com>
2023-11-17 01:10:00 -0300

    changes: Tentativa de criar uma função reutilizável para o changelog

--- src/pages/sobre/_lib/commitList.ts
+++ src/pages/sobre/_lib/commitList.ts
@@ -1,111 +1,125 @@
-import dayjs from 'dayjs'
+import dayjs from "dayjs";
 
 export type Commit = {
   commit: {
-    message: string
+    message: string;
     author: {
-      date: string
-    }
-  }
-}
-export type CommitsByDate = { [date: string]: Commit[] }
+      date: string;
+    };
+  };
+};
+export type CommitsByDate = { [date: string]: Commit[] };
+
+const types = {
+  add: "additions" as const,
+  remove: "removals" as const,
+  changes: "changes" as const,
+  other: "other" as const,
+};
+
+type ValueOf<T> = T[keyof T];
+
 export type CommitsByType = {
-  additions: Commit[]
-  removals: Commit[]
-  changes: Commit[]
-  other: Commit[]
-}
+  [key in ValueOf<typeof types>]: Commit[];
+};
+
 export type CommitsByDateAndType = {
-  [date: string]: CommitsByType
-}
+  [date: string]: CommitsByType;
+};
 
 async function getCommitsFromGithub() {
   const commits = await fetch(
-    'https://api.github.com/repos/br-dev-org/site/commits',
+    "https://api.github.com/repos/br-dev-org/site/commits",
     {
-      method: 'GET',
+      method: "GET",
       headers: {
-        Accept: 'application/vnd.github+json',
-        Authorization: 'Bearer ' + import.meta.env.GITHUB_TOKEN
-      }
+        Accept: "application/vnd.github+json",
+        Authorization: "Bearer " + import.meta.env.GITHUB_TOKEN,
+      },
     }
-  ).then<Commit[]>(response => response.json())
+  ).then<Commit[]>((response) => response.json());
 
   if (!Array.isArray(commits)) {
-    console.error("Something went wrong on fetching commits", commits)
-    return []
+    console.error("Something went wrong on fetching commits", commits);
+    return [];
   }
 
-  return commits
+  return commits;
 }
 
 export async function getCommitListOrganizedByDateAndType(): Promise<CommitsByDateAndType> {
-  const commits = await getCommitsFromGithub()
+  const commits = await getCommitsFromGithub();
 
   const commitsByDate = commits
-    .map<Commit & { date: string }>(commit => ({
+    .map<Commit & { date: string }>((commit) => ({
       ...commit,
-      date: dayjs(commit.commit.author.date).format('YYYY-MM-DD')
+      date: dayjs(commit.commit.author.date).format("YYYY-MM-DD"),
     }))
     .reduce((commitsByDate, commit) => {
-      const date = commit.date
+      const date = commit.date;
 
       if (date in commitsByDate) {
         return {
           ...commitsByDate,
-          [date]: [...commitsByDate[date], commit]
-        }
+          [date]: [...commitsByDate[date], commit],
+        };
       }
 
       return {
         ...commitsByDate,
-        [date]: [commit]
-      }
-    }, {} as CommitsByDate)
+        [date]: [commit],
+      };
+    }, {} as CommitsByDate);
+
+  const aggregateByType = (
+    type: keyof typeof types,
+    commitsByType: CommitsByType,
+    commit: Commit
+  ) => {
+    const message = commit.commit.message;
+
+    if (new RegExp(`^${type}: `).test(message)) {
+      return {
+        ...commitsByType,
+        [types[type]]: [...commitsByType[types[type]], commit],
+      };
+    }
+
+    return;
+  };
 
   const commitsByDateAndType = Object.keys(commitsByDate).reduce(
     (commitsByDateAndType, date) => ({
       ...commitsByDateAndType,
       [date]: commitsByDate[date].reduce(
         (commitsByType, commit) => {
-          const message = commit.commit.message
+          for (const type of Object.keys(types)) {
+            const item = aggregateByType(
+              type as unknown as keyof typeof types,
+              commitsByType,
+              commit
+            );
 
-          if (/^add: /.test(message)) {
-            return {
-              ...commitsByType,
-              additions: [...commitsByType.additions, commit]
-            }
-          }
-
-          if (/^remove: /.test(message)) {
-            return {
-              ...commitsByType,
-              removals: [...commitsByType.removals, commit]
-            }
-          }
-
-          if (/^changes*: /.test(message)) {
-            return {
-              ...commitsByType,
-              changes: [...commitsByType.changes, commit]
+            if (item) {
+              return item;
             }
           }
 
           return {
             ...commitsByType,
-            other: [...commitsByType.other, commit]
-          }
+            other: [...commitsByType.other, commit],
+          };
         },
         {
           additions: [],
           removals: [],
           changes: [],
-          other: []
+          other: [],
         } as CommitsByType
-      )
+      ),
     }),
     {} as CommitsByDateAndType
-  )
+  );
 
-  return commitsByDateAndType
+  return commitsByDateAndType;
 }
